ccc501acc300b3d6487ad1eb3eb734bf60821272
Javier Cabanillas <jcabanillas@bitevolution.net>
2017-05-29 17:45:47 -0500

    Se cambian las vistas de los widgets

--- widgets/views/hotPosts.php
+++ widgets/views/hotPosts.php
@@ -1,17 +1,22 @@
 <?php
 use yii\helpers\Html;
+
 ?>
-<div class="portlet">
-    <div class="portlet-decoration">
-        <div class="portlet-title"><?= $title ?></div>
+<div class="c-content-ver-nav">
+    <div class="c-content-title-1 c-theme c-title-md c-margin-t-40">
+        <h3 class="c-font-bold c-font-uppercase"><?= $title ?></h3>
+        <div class="c-line-left c-theme-bg"></div>
     </div>
-    <div class="portlet-content">
-        <ul>
-            <?php foreach($posts as $post): ?>
-                <li>
+    <ul class="c-content-recent-posts-1">
+        <?php foreach ($posts as $post): ?>
+            <li>
+                <div class="c-image">
+                    <img src="<?= '/' . $post->banner ?>" alt="" class="img-responsive"></div>
+                <div class="c-post">
                     <?php echo Html::a(Html::encode($post->title), $post->getUrl()); ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
+                </div>
+            </li>
+        <?php endforeach; ?>
+
+    </ul>
 </div>
--- widgets/views/search.php
+++ widgets/views/search.php
@@ -1,6 +1,13 @@
-<form  action="<?php echo Yii::$app->getUrlManager()->createUrl(["/blog/default/index"])?>" method="get">
-    <div class="search">
-        <input type="text" name="keyword" id="keyword" value="<?php if(isset($_GET['keyword']) && strlen($_GET['keyword'])>0) echo $_GET['keyword']; else echo 'palabra clave'; ?>" class="search_input" onblur="if(this.value=='') this.value='palabra clave';"onfocus="if(this.value=='palabra clave') this.value='';"/>
-        <input type="submit" class="search_submit"  id="search_submit" value="Buscar"/>
+<form action="<?php echo Yii::$app->getUrlManager()->createUrl(["/blog/default/index"]) ?>" method="get">
+    <div class="search input-group">
+        <input type="text" class="form-control c-square c-theme-border" name="keyword" id="keyword"
+               value="<?php if (isset($_GET['keyword']) && strlen($_GET['keyword']) > 0) echo $_GET['keyword']; else echo 'palabra clave'; ?>"
+               class="search_input" onblur="if(this.value=='') this.value='palabra clave';"
+               onfocus="if(this.value=='palabra clave') this.value='';"/>
+        <span class="input-group-btn">
+                                        <input type="submit"
+                                               class="search_submit btn c-theme-btn c-theme-border c-btn-square"
+                                               id="search_submit" value="Buscar"/>
+                                    </span>
     </div>
 </form>
